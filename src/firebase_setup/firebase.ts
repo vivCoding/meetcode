95fdf784a2b97df134d625ab816cd63f271f5e08
Aaryan Gautam <64756497+aaryan-gautam@users.noreply.github.com>
2023-01-21 08:24:57 -0500

    add firestore auth firebase database

--- src/firebase_setup/firebase.ts
+++ src/firebase_setup/firebase.ts
@@ -1,8 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { getAnalytics } from "firebase/analytics"
 import { initializeApp } from "firebase/app"
-import { ref, set } from "firebase/database"
-import { getDatabase } from "firebase/database"
 import { getFirestore } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,19 +7,23 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_4tdcZyCAXTgb7nYXmRTfre5YBQJpDeM",
-  authDomain: "ternary-search-438fe.firebaseapp.com",
-  projectId: "ternary-search-438fe",
-  storageBucket: "ternary-search-438fe.appspot.com",
-  messagingSenderId: "831440717730",
-  appId: "1:831440717730:web:92408046a02ede908254fb",
-  measurementId: "G-57SZHY0JH4",
+  apiKey: "AIzaSyB0JN2YLV53lea3yVmTp-ZaYK8fM3A7reA",
+  authDomain: "ternary-search.firebaseapp.com",
+  databaseURL: "https://ternary-search-default-rtdb.firebaseio.com",
+  projectId: "ternary-search",
+  storageBucket: "ternary-search.appspot.com",
+  messagingSenderId: "456296834940",
+  appId: "1:456296834940:web:72a3167dc3457a480c66f9",
+  measurementId: "G-Z0GZZGDSZ1",
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 // const analytics = getAnalytics(app)
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(app)
 // export const db = getFirestore(app)
 // export const dbRef = ref(getDatabase(), "rooms")
-export const db = getDatabase(app)
+export const db = getFirestore(app)
 // const reference = ref(db, "rooms/" + roomId)
