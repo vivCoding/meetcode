ebc631bd046b3d24d35635a9be85486550f9270a
Aaryan Gautam <64756497+aaryan-gautam@users.noreply.github.com>
2023-01-21 13:35:47 -0500

    added crud methods for firebase

--- src/types/room.ts
+++ src/types/room.ts
@@ -6,7 +6,10 @@ export type Problem = {
 
 export type Settings = {
   mode: string
-  problem: Problem
+  // problem: Problem
+  problem_tags: string[]
+  problem_difficulty: string
+  problem_list_tag: string
   number_of_questions: number
   time_limit: number
   time_limit_total: number
@@ -16,10 +19,11 @@ export type Settings = {
 }
 
 export type Room = {
-  room_settings: Settings
+  room_settings?: Settings
   question_queue: string[]
   member_list: string[]
-  leaderboard: { string: number }[]
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  leaderboard: any[]
   admin: string
   problem_complete_status: string
 }
