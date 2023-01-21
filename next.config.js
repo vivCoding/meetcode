c568b6807df09228f1df1d1fb0726e4c1ea633a3
vivCoding <vivCoding@gmail.com>
2023-01-20 19:16:05 -0500

    jazz

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // NOTE to disable double rerendering/console logs
+  reactStrictMode: false,
+  webpack(config, options) {
+    config.resolve.alias["@mui/material"] = "@mui/joy"
+    return config
+  }
 }
 
 module.exports = nextConfig
